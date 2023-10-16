fdd3ddfe47137a78f3f8b095740b9033e499cf6c
TyRoopam9599 <roopamtyagi72@gmail.com>
2023-10-16 08:14:31 +0530

    Optimize Middlewares

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,23 +1,23 @@
-import JWT from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
-export const authMiddleware = async (req, res, next) => {
+export const authMiddleware = (req, res, next) => {
   try {
-    const token = await req.headers["authorization"].split(" ")[1];
+    const token = req.headers["authorization"]?.split(" ")[1];
     if (!token) return res.status(401).json({ message: "Access Denied" });
-    JWT.verify(token, process.env.JWT, (err, decoded) => {
+
+    jwt.verify(token, process.env.JWT, (err, decoded) => {
       if (err) {
-        return res.status(403).send({
+        return res.status(403).json({
           message: "Token verification failed",
           success: false,
         });
-      } else {
-        req.user = decoded;
-        next();
       }
+      req.user = decoded;
+      next();
     });
   } catch (error) {
-    res.status(500).send({
-      message: "Internkffghjkghjal Server Error",
+    res.status(500).json({
+      message: "Internal Server Error",
       success: false,
     });
   }
